312137dcb01606ef060893e53a4c436c7a3bdc0c
Francisca Pessanha <mariafranciscapessanha@gmail.com>
2019-01-02 15:54:02 +0000

    Fix bug on the dataset division

--- keras-vgg16-classification/get_datasets.py
+++ keras-vgg16-classification/get_datasets.py
@@ -107,7 +107,7 @@ Arguments:
     * i: image index
     * folder_name: name of the dataset
 """
-def save_image(image, label, i, folder_name):
+def save_image(path, image, label, folder_name):
 
     curr_path = os.getcwd()
     path_arrabida = os.path.normpath(os.path.join(curr_path, "../dataset/divided_sets/", folder_name, "arrabida"))
@@ -118,33 +118,43 @@ def save_image(image, label, i, folder_name):
 
     if not os.path.exists(path_arrabida):
         os.makedirs(path_arrabida)
-
+    else: 
+        os.system('rm '+ path_arrabida + "/*")
+    
     if not os.path.exists(path_camara):
         os.makedirs(path_camara)
-
+    else: 
+        os.system('rm '+ path_camara + "/*")
+    
     if not os.path.exists(path_clerigos):
         os.makedirs(path_clerigos)
-
+    else: 
+        os.system('rm '+ path_clerigos + "/*")
+        
     if not os.path.exists(path_musica):
             os.makedirs(path_musica)
+    else: 
+        os.system('rm '+ path_musica + "/*")
 
     if not os.path.exists(path_serralves):
             os.makedirs(path_serralves)
+    else: 
+        os.system('rm '+ path_serralves + "/*")
 
     if label == 0: #arrabida
-        filename = (path_arrabida + "/arrabida_%d.jpg") % i
+        filename = path_arrabida + path[path.rindex('/'):]
         cv.imwrite(filename, image)
     elif label == 1: #camara
-        filename = (path_camara + "/camara_%d.jpg") % i
+        filename = path_camara + path[path.rindex('/'):]
         cv.imwrite(filename, image)
     elif label == 2: #clerigos
-        filename = (path_clerigos + "/clerigos_%d.jpg") % i
+        filename = path_clerigos + path[path.rindex('/'):]
         cv.imwrite(filename, image)
     elif label == 3: #musica
-        filename = (path_musica + "/musica_%d.jpg") % i
+        filename = path_musica + path[path.rindex('/'):]
         cv.imwrite(filename, image)
     elif label == 4: #serralves
-        filename = (path_serralves + "/serralves_%d.jpg") % i
+        filename = path_serralves + path[path.rindex('/'):]
         cv.imwrite(filename, image)
 
 """
@@ -158,7 +168,7 @@ Arguments:
     * i: image index
     * folder_name: name of the dataset
 """
-def save_copy(path, label, i, folder_name, extension):
+def save_copy(path, label, folder_name):
 
     curr_path = os.getcwd()
     path_arrabida = os.path.normpath(os.path.join(curr_path, "../dataset/divided_sets/", folder_name, "arrabida"))
@@ -169,33 +179,43 @@ def save_copy(path, label, i, folder_name, extension):
     
     if not os.path.exists(path_arrabida):
         os.makedirs(path_arrabida)
-
+    else: 
+        os.system('rm '+ path_arrabida + "/*")
+    
     if not os.path.exists(path_camara):
         os.makedirs(path_camara)
-
+    else:
+        os.system('rm '+ path_camara+ "/*")
+    
     if not os.path.exists(path_clerigos):
         os.makedirs(path_clerigos)
+    else:
+        os.system('rm '+ path_clerigos + "/*")
 
     if not os.path.exists(path_musica):
         os.makedirs(path_musica)
+    else:
+        os.system('rm '+ path_musica + "/*")
 
     if not os.path.exists(path_serralves):
         os.makedirs(path_serralves)
+    else:
+        os.system('rm '+ path_serralves + "/*")
 
     if label == 0: #arrabida
-        filename = (path_arrabida + "/arrabida_%d" + extension) % i
+        filename = path_arrabida + path[path.rindex('/'):]
         shutil.copy2(path,filename)
     elif label == 1: #camara
-        filename = (path_camara + "/camara_%d" + extension) % i
+        filename = path_camara + path[path.rindex('/'):]
         shutil.copy2(path,filename)
     elif label == 2: #clerigos
-        filename = (path_clerigos + "/clerigos_%d" + extension) % i
+        filename = path_clerigos + path[path.rindex('/'):]
         shutil.copy2(path,filename)
     elif label == 3: #musica
-        filename = (path_musica + "/musica_%d" + extension) % i
+        filename = path_musica + path[path.rindex('/'):]
         shutil.copy2(path,filename)
     elif label == 4: #serralves
-        filename = (path_serralves + "/serralves_%d" + extension) % i
+        filename = path_serralves + path[path.rindex('/'):]
         shutil.copy2(path,filename)
 """
 Get Data
@@ -203,73 +223,74 @@ Get Data
 Saves datasets after resizing.
 
 """            
-def run():
-    curr_path = os.getcwd()
 
-    print("1. Get images paths")
-    arrabida = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/arrabida')))
-    camara = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/camara')))
-    clerigos = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/clerigos')))
-    musica = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/musica')))
-    serralves = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/serralves')))
-    x = np.concatenate((arrabida, camara, clerigos, musica, serralves))
-    
-    y_arrabida = np.zeros(np.shape(arrabida),np.uint8)
-    y_camara = np.ones(np.shape(camara),np.uint8)
-    y_clerigos = np.ones(np.shape(clerigos),np.uint8)*2
-    y_musica = np.ones(np.shape(musica),np.uint8)*3
-    y_serralves = np.ones(np.shape(serralves),np.uint8)*4
-    y = np.concatenate((y_arrabida, y_camara, y_clerigos, y_musica, y_serralves))
-    
-    print("2. Split into datasets (train, test, validation)")
-    x_train, y_train, x_val, y_val, x_test, y_test = split_data(x,y)
-    
+curr_path = os.getcwd()
 
-    print("3. Save resized images by dataset")
-    print("  3.1 Train")
-    for path,i in zip(x_train, range(len(x_train))):
-        image = cv.imread(path[0])
-        image = resize_images(image)
-        save_image(image, y_train[i],i, "resized_train")
+print("1. Get images paths")
+arrabida = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/arrabida')))
+camara = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/camara')))
+clerigos = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/clerigos')))
+musica = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/musica')))
+serralves = find_images(os.path.normpath(os.path.join(curr_path, '../dataset/images/serralves')))
+x = np.concatenate((arrabida, camara, clerigos, musica, serralves))
+
+y_arrabida = np.zeros(np.shape(arrabida),np.uint8)
+y_camara = np.ones(np.shape(camara),np.uint8)
+y_clerigos = np.ones(np.shape(clerigos),np.uint8)*2
+y_musica = np.ones(np.shape(musica),np.uint8)*3
+y_serralves = np.ones(np.shape(serralves),np.uint8)*4
+y = np.concatenate((y_arrabida, y_camara, y_clerigos, y_musica, y_serralves))
+
+print("2. Split into datasets (train, test, validation)")
+x_train, y_train, x_val, y_val, x_test, y_test = split_data(x,y)
+
+
+print("3. Save resized images by dataset")
+print("  3.1 Train")
+for path,i in zip(x_train, range(len(x_train))):
+    image = cv.imread(path[0])
+    image = resize_images(image)
+    save_image(path[0], image, y_train[i], "resized_train")
    
-    print("  3.2 Validation")
-    for path,i in zip(x_val, range(len(x_val))):
-        image = cv.imread(path[0])
-        image = resize_images(image)
-        save_image(image, y_val[i],i, "resize_val")
+print("  3.2 Validation")
+for path,i in zip(x_val, range(len(x_val))):
+    image = cv.imread(path[0])
+    image = resize_images(image)
+    save_image(path[0], image, y_val[i], "resized_val")
    
-    print("  3.3 Test")
-    for path,i in zip(x_test, range(len(x_test))):
-        image = cv.imread(path[0])
-        image = resize_images(image)
-        save_image(image, y_test[i],i, "resize_test")
-    
+print("  3.3 Test")
+for path,i in zip(x_test, range(len(x_test))):
+    image = cv.imread(path[0])
+    image = resize_images(image)
+    save_image(path[0], image, y_test[i], "resized_test")
+
+
+#ACRESCENTAR ANOTAÇÕES QUE FALTAM
+print("4. Save original images by dataset")
+print("  4.1 Train")
+for path,i in zip(x_train, range(len(x_train))):
+    ann_path = path[0].replace("images", "annotations")
+    ann_path = ann_path.replace("jpg", "xml")
+    if os.path.exists(ann_path):
         
-    #ACRESCENTAR ANOTAÇÕES QUE FALTAM
-    print("4. Save original images by dataset")
-    print("  4.1 Train")
-    for path,i in zip(x_train, range(len(x_train))):
-        ann_path = path[0].replace("images", "annotations")
-        ann_path = ann_path.replace("jpg", "xml")
-        if os.path.exists(ann_path):
-            save_copy(ann_path, y_train[i], i, "annotations_train", ".xml")
-            save_copy(path[0], y_train[i], i, "original_train", ".jpg")
+        save_copy(ann_path, y_train[i], "annotations_train")
+        save_copy(path[0], y_train[i], "original_train")
    
-    print("  4.2 Validation")
-    for path,i in zip(x_val, range(len(x_val))):
-        ann_path = path[0].replace("images", "annotations")
-        ann_path = ann_path.replace("jpg", "xml")
-        if os.path.exists(ann_path):
-            save_copy(ann_path, y_val[i], i, "annotations_val", ".xml")
-            save_copy(path[0], y_val[i], i, "original_val", ".jpg")
+print("  4.2 Validation")
+for path,i in zip(x_val, range(len(x_val))):
+    ann_path = path[0].replace("images", "annotations")
+    ann_path = ann_path.replace("jpg", "xml")
+    if os.path.exists(ann_path):
+        save_copy(ann_path, y_val[i], "annotations_val")
+        save_copy(path[0], y_val[i], "original_val")
 
-    print("  4.3 Test")
-    for path,i in zip(x_test, range(len(x_test))):
-        ann_path = path[0].replace("images", "annotations")
-        ann_path = ann_path.replace("jpg", "xml")
-        if os.path.exists(ann_path):
-            save_copy(ann_path, y_test[i], i, "annotations_test", ".xml")
-            save_copy(path[0], y_test[i], i, "original_test", ".jpg")
-run()
+print("  4.3 Test")
+for path,i in zip(x_test, range(len(x_test))):
+    ann_path = path[0].replace("images", "annotations")
+    ann_path = ann_path.replace("jpg", "xml")
+    if os.path.exists(ann_path):
+        save_copy(ann_path, y_test[i], "annotations_test")
+        save_copy(path[0], y_test[i], "original_test")
+
 
 
